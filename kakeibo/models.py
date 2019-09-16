a77de838c523b8c7bf9e15381cfa415d95efb8e6
Your Name <you@example.com>
2019-09-16 19:48:28 +0900

    Modified templates to display posts from database.

--- kakeibo/models.py
+++ kakeibo/models.py
@@ -3,31 +3,27 @@ from datetime import datetime
 
 class Category(models.Model):
    class Meta:
-      #テーブル名の指定
+      #テーブル名の定義
       db_table ="category"
       verbose_name ="カテゴリ"
       verbose_name_plural ="カテゴリ"
 
    #カラム名の定義
    category_name = models.CharField(max_length=255,unique=True)
-   #ここから追加
    def __str__(self):
        return self.category_name
 
 class Kakeibo(models.Model):
-
    class Meta:
-       #テーブル名
+       #テーブル名の定義
        db_table ="kakeibo"
        verbose_name ="家計簿"
        verbose_name_plural ="家計簿"
-
    #カラムの定義
    date = models.DateField(verbose_name="日付",default=datetime.now)
+   #外部キーの設定
    category = models.ForeignKey(Category, on_delete = models.PROTECT, verbose_name="カテゴリ")
    money = models.IntegerField(verbose_name="金額", help_text="単位は日本円")
    memo = models.CharField(verbose_name="メモ", max_length=500)
-
-
    def __str__(self):
        return self.memo
--- kakeibo/views.py
+++ kakeibo/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-from . forms import KakeiboForm  #forms.pyからKakeiboFormをインポート
+from . forms import KakeiboForm
 from django.urls import reverse_lazy
 # Create your views here.
-
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from .models import Category, Kakeibo
+from django.db import models
 
 #一覧表示用のDjango標準ビュー(ListView)を承継して一覧表示用のクラスを定義
 class KakeiboListView(ListView):
    #利用するモデルを指定
    model = Kakeibo
-   #データを渡すテンプレートファイルを指定
+   #テンプレートファイルの指定
    template_name = 'kakeibo/kakeibo_list.html'
    #家計簿テーブルの全データを取得するメソッドを定義
    def queryset(self):
@@ -19,7 +19,7 @@ class KakeiboListView(ListView):
 class KakeiboCreateView(CreateView):
     #利用するモデルを指定
     model = Kakeibo
-    #利用するフォームクラス名を指定
+    #利用するフォームの指定
     form_class = KakeiboForm
     #登録処理が正常終了した場合の遷移先を指定
     success_url = reverse_lazy('kakeibo:create_done')
@@ -45,3 +45,31 @@ class KakeiboDeleteView(DeleteView):
     success_url = reverse_lazy('kakeibo:delete_done')
 def delete_done(request):
     return render(request, 'kakeibo/delete_done.html')
+
+def show_circle_grahp(request):
+    kakeibo_data = Kakeibo.objects.all()
+    #登録されている金額の合計を求める処理
+    total = 0
+    for item in  kakeibo_data:
+        total += item.money
+    category_list =[]
+    #カテゴリデータを全権取得する
+    category_data = Category.objects.all()
+    #カテゴリの内容をリスト形式で格納
+    for item in category_data:
+        category_list.append(item.category_name)
+    #カテゴリごとの合計金額を求める処理
+    category_dict = {}
+    for i,item in enumerate(category_list):
+        category_total = Kakeibo.objects.filter(category__category_name=category_list[i])\
+        .aggregate(sum=models.Sum('money'))['sum']
+        if category_total != None:
+            ratio = int((category_total / total) *100)
+            category_dict[item] = ratio
+        else:
+            ratio = 0
+            category_dict[item] = ratio
+
+    return render(request, 'kakeibo/kakeibo_circle.html',{
+        'category_dict': category_dict,
+         } )
